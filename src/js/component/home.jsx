93087660dfac3b1bbb288ee34581a3ffb1bd11c1
Truitt Janney <153782672+truittjanney@users.noreply.github.com>
2024-03-23 01:03:49 +0000

    project complete

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,20 +1,75 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-
-
-//create your first component
+//first component
 const Home = () => {
 	const [ inputValue, setInputValue ] = useState("");
 	const [ tasks, setTasks ] = useState([]);
 
-	function saveTasks (e) {
+	async function fetchToDos() {
+
+		const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/truitt', { method: "GET"})
+		const data = await response.json()
+		setTasks(data)
+			console.log(data);
+		}
+
+	useEffect(() => {
+		fetchToDos();
+	}, [])
+
+// function to save tasks
+function saveTasks(e) {
 		if (e.key=="Enter") {
-			setTasks([...tasks, inputValue])
+			const newTasks = ([...tasks, {label: inputValue, done: false}]);
+			fetch("https://playground.4geeks.com/apis/fake/todos/user/truitt", {
+				method: "PUT",
+				body: JSON.stringify(newTasks),
+				headers: {
+				"Content-Type": "application/json",
+				},
+		  });
+
+			setTasks(newTasks);
 			setInputValue("")
 		}
 	}
 
+// function to update one deleted task (attempt)
+function deleteTask(taskId) {
+	const updatedTasks = tasks.filter(task => task.id !== taskId);
+	setTasks(updatedTasks);
+
+		fetch('https://playground.4geeks.com/apis/fake/todos/user/truitt', {
+			method: "PUT",
+			body: JSON.stringify (updatedTasks),
+			headers: {
+			"Content-Type": "application/json"
+			}
+		})
+		.then(response => {
+			if (!response.ok) {
+				throw new Error ('HTTP error! status: ${response.status}');
+			}
+			console.log("Task deleted successfully from server!");
+		})
+		.catch(error => {
+			console.log("Error deleting task:", error);
+		});
+}
+
+// function to update and delete all tasks
+function deleteAllTasks() {
+	const removeAllTasks = ([...tasks, {label: inputValue, done:false}]);
+		fetch('https://playground.4geeks.com/apis/fake/todos/user/truitt'), {
+			method: "PUT",
+			body: JSON.stringify(tasks),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		};
+	}
+
+// the to-do list
 	return (
 		<div className="container">
 			<h1>Today's Agenda:</h1>
@@ -28,14 +83,11 @@ const Home = () => {
 
 					placeholder="task" />
 				</li>
-					{tasks.map((item, index) => (
+					{tasks.map((item) => (
 						<li>
-							{item}{" "}
+							{item.label}{" "}
 							<i class="fas fa-trash-alt"
-							onClick={() =>
-								setTasks(
-									tasks.filter((t, currentIndex) => index != currentIndex)
-								)}
+							onClick={() => deleteTask(item.id)}
 							></i>
 						</li>
 					))}
@@ -43,4 +95,5 @@ const Home = () => {
 			<div>{tasks.length}</div>
 		</div>)
 }
+
 export default Home;
